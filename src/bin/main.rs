4b585d8b892d428806574014614764d50dd7181e
Sefa Osmanoğlu <sfsmnl@gmail.com>
2024-07-28 01:49:28 +0300

    trying

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,23 @@
+use core::str;
+use std::{io, io::Write, thread};
+use std::time::Duration;
+use lipsum::lipsum;
+
+/// Prints the given string like a keyboard, character by character, with
+/// random delays between each character.
+///
+/// # Arguments
+///
+/// * `s` - The string to print.
+pub fn print_like_keyboard(s: &str) {
+    for c in s.chars() {
+        print!("{}", c);
+        io::stdout().flush().unwrap();
+        thread::sleep(Duration::from_millis(rand::random::<u64>() % 200));
+    }
+    println!();
+}
+
 fn main() {
-    
+    print_like_keyboard(&lipsum(20));
 }
